7c2f71f26f746ed570253633a188b8e2ba15ee61
iamsuperdupercool <github@lustrouscloud.xyz>
2024-09-12 12:13:07 +0000

    Change implementation to utilize SHA-3 as BLAKE3 sucks

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::io;
 use std::io::Write;
 use std::{thread, time::Duration};
-use blake3;
-use fastrand;
-
+use getrandom;
+use sha3::{Digest, Sha3_256};
+use hex;
 fn main() {
     // Get a lock on stdout
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
-    let mut hash_content;
-    let mut hash_content_u8;
+    let mut hasher = Sha3_256::new();
+    let mut rbuf = [0u8; 32];
+    let mut random_hash;
     loop {
-        hash_content = fastrand::i32(..);
-        hash_content_u8 = hash_content.to_ne_bytes();
-        let random_hash = blake3::hash(&hash_content_u8);
+        getrandom::getrandom(&mut rbuf).unwrap();
+        hasher.update(rbuf);
+        random_hash = hex::encode(hasher.finalize_reset());
         write!(stdout, "{}\n", random_hash).unwrap();
         stdout.flush().unwrap();
         thread::sleep(Duration::from_millis(5));
